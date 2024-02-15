4840596f4341c87325fcab48800d15bee4ec909e
Luc4sL4idens <lucaslaidens20@gmail.com>
2024-02-15 19:44:02 -0300

    aula web2

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({message: "penis"})
+})
+
+app.get('/produto', (req, res) => {
+    res.json({message: "socorro"})
+  })
+
+app.listen(port, () => {
+  console.log(`o servidor está rodando em ${port}`)
+})
