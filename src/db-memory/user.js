20e91672546d438cfbc6543deb25cf55cb544e51
Luc4sL4idens <lucaslaidens20@gmail.com>
2024-02-15 22:11:08 -0300

    1

--- src/db-memory/user.js
+++ src/db-memory/user.js
@@ -1,13 +1,13 @@
 export const users = [
     {
         id: 1,
-        name: 'Renan Cavichi',
-        email: 'renancavichi@gmail.com',
+        name: 'Lucas Laidens',
+        email: 'lucaslaidens20@gmail.com',
         avatar: 'https://avatars.githubusercontent.com/u/4259630?v=4'
     },
     {
         id: 2,
-        name: 'Taku Amado',
+        name: 'S',
         email: 'takuamado@gmail.com',
         avatar: 'https://avatars.githubusercontent.com/u/30598?v=4'
     },
--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
 import express from 'express'
-import {PORT,HOST} from './config.js'
+import {PORT, HOST} from './config.js'
+import {users} from './db-memory/user.js'
+
 
 const app = express()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({message: "hello word"})
 })
 
 app.get('/produto', (req, res) => {
-    res.json({message: "socorro"})
+    res.json({message: "caixa de leite"})
+})
+
+app.get('/user', (req, res) => {
+  res.json({
+    success: "usuarios listados com sucesso",
+    users
   })
+})
+
+app.post('/user', (req, res) => {
+  const user = req.body
+  user.id = users[users.length - 1].id + 1
+  users.push(user)
+  console.log(user)
+  res.json({
+    success: "usuarios listados com sucesso",
+    users
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`o servidor está rodando em ${HOST}:${PORT}`)
